db1821026bbf3ee599f80fcac4e57fec0acdfdd3
Fernanda Guilherme <eufernandagui154@hotmail.com>
2024-02-22 14:37:09 -0300

    :tada: Inicia o frontend.

---
+++ frontend/dreamy-decor/src/components/Header/nav-page.vue
@@ -0,0 +1,31 @@
+<template>
+      <v-layout>
+        <v-app-bar>
+          <template v-slot:prepend>
+          </template>
+  
+          <v-app-bar-title>Dreamy Decor Lines</v-app-bar-title>
+  
+          <template v-slot:append>
+            <svg-icon type="mdi" :path="bag" class="mr-4"></svg-icon>
+            <svg-icon type="mdi" :path="user"></svg-icon>
+          </template>
+        </v-app-bar>
+      </v-layout>
+  </template>
+<script>
+  import SvgIcon from '@jamescoyle/vue-icon';
+  import { mdiShoppingOutline, mdiAccount } from '@mdi/js';
+
+  export default {
+    components: {
+      SvgIcon
+    },
+    data() {
+      return {
+        bag: mdiShoppingOutline,
+        user: mdiAccount
+      }
+    }
+  }
+</script>
