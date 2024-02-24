45db3f236a52b3829811d3b2f7fc6c68d51270cc
Fernanda Guilherme <eufernandagui154@hotmail.com>
2024-02-23 23:36:15 -0300

    Adiciona arquivo insomnia para teste.

--- backend/data-source.ts
+++ backend/data-source.ts
@@ -4,7 +4,7 @@ export const AppDataSource = new DataSource({
   type: "mysql",
   host: "127.0.0.1",
   port: 3306,
-  username: "fernanda",
+  username: "Fernanda",
   password: "123",
   database: "Dreamy",
   entities: ["src/entities/*.ts"],
